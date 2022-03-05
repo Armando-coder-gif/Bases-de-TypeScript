6af6837b894eb3b964ea382aef3a2b25350554bf
Armando <armando.gonzalez@correo.unimet.edu.ve>
2022-03-04 19:58:22 -0600

    Desestructuración de objetos

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,42 @@
     ===== Código de TypeScript =====
 */
 
+interface Detalle{
+    autor: string;
+    year: number;
+}
+
+
+interface Reproductor{
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalle;
+}
+
+const reproductor: Reproductor = {
+    volumen: 50,
+    segundo: 75,
+    cancion: 'Te quiero más',
+    detalles: {
+        autor: 'Enghel',
+        year: 2016,
+    },
+} 
+
+// Desestructuración
+const { volumen: vol, segundo, cancion, detalles } = reproductor;
+const { autor, year } = detalles;
+
+// o se puede hacer de esta manera
+
+console.log( 'El volumen actual es de: ', vol );
+console.log( 'El segundo actual es: ', segundo  );
+console.log( 'La canción es: ', cancion  );
+console.log( 'El autor es: ', autor );
+console.log( 'El año es: ', year );
+
+/*
+si quiero cambiar el nombre de la variable 
+entonces coloco volumen: vol
+*/
